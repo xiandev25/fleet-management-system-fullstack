384c0ea2f3b5a8d217fe306a907b13b6dde99e65
Xian Dev <xiandev25@gmail.com>
2026-05-14 02:24:51 +0100

    chore: Update App.vue to use router-view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <HelloWorld />
-</template>
+<style>
+#app {
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>

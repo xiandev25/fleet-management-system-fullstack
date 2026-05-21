826a39ef103a186f22649d30848c23f6d848af07
Xian Dev <xiandev25@gmail.com>
2026-05-21 21:51:38 +0100

    fix(fd): Remove unnecessary BusStopSerializer fields
    Those were Blocking BusLine objects to be created/updated.

--- backend/routes/serializers.py
+++ backend/routes/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import BusLine, BusStop, Shuttle
 
+
 class BusStopSerializer(serializers.ModelSerializer):
     class Meta:
         model = BusStop
-        fields = ['id', 'line', 'name', 'sequence_number', 'scheduled_time', 'location_lat', 'location_lon']
+        fields = ["id", "line", "name", "sequence_number", "scheduled_time"]
+
 
 class BusLineSerializer(serializers.ModelSerializer):
     # Nesting the stops inside the line for a complete view
@@ -12,9 +14,10 @@ class BusLineSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BusLine
-        fields = ['id', 'name', 'description', 'stops']
+        fields = ["id", "name", "description", "stops"]
+
 
 class ShuttleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shuttle
-        fields = ['id', 'name', 'pickup_point', 'scheduled_time', 'location_lat', 'location_lon']
+        fields = ["id", "name", "pickup_point", "scheduled_time"]
